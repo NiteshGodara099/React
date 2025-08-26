f7e9e39e313c394a4e44eefe0f9fec266c4faa00
NiteshGodara099 <133318242+NiteshGodara099@users.noreply.github.com>
2025-08-26 20:37:12 +0530

    Added CounterProject

---
+++ CounterProject/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(15)
+
+  // let counter = 5;  
+  
+  const addValue = () => {
+    if(counter<20){
+      setCounter(counter + 1)
+    }
+  };
+  
+  const removeValue = () => {
+    if(counter>0){
+      setCounter(counter - 1)
+    }
+  }
+
+  return (
+    <>
+      <h1>React</h1>
+      <h3>Counter Value: {counter}</h3>
+      <button 
+      onClick={addValue}>Add Value</button>
+      <br/>
+      <br/>
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
